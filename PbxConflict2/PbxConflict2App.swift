5655ba4cd4ad28e672b894cea23e6ee1a31d7d3f
jimmy <wjdtjq6@gmail.com>
2024-10-08 11:45:42 +0900

    Initial Commit

---
+++ PbxConflict2/PbxConflict2App.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflict2App.swift
+//  PbxConflict2
+//
+//  Created by 소정섭 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflict2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
